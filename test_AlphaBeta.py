7f3c8094ca589ec20a4ea5e7825dea6a834366c6
AGottu <45110948+AGottu@users.noreply.github.com>
2018-12-07 19:52:17 -0800

    alphaBeta script

---
+++ test_AlphaBeta.py
@@ -0,0 +1,42 @@
+import othello
+from alphaBeta import alphaBeta
+
+
+def main():
+    board = othello.Board()
+
+    for i in range(8):
+        for j in range(8):
+            board.force_place_symbol((i, j), '0')
+
+    board.force_place_symbol((4, 4), 'B')
+    board.force_place_symbol((5, 5), 'B')
+
+    board.force_place_symbol((4, 5), 'W')
+    board.force_place_symbol((5, 4), 'W')
+
+    board.print()
+    print("--------------")
+
+    while True:
+
+        move_b = alphaBeta(state=board, depth=3, player='B')
+        if move_b is not None:
+            board.make_move(move_b, 'B', play_test=False)
+        board.print()
+        print()
+
+        move_w = alphaBeta(state=board, depth=3, player='W')
+        if move_w is not None:
+            board.make_move(move_w, 'W', play_test=False)
+        board.print()
+        print()
+
+        if move_b is None and move_w is None:
+            break
+
+    print(board.get_winner() + ' wins!')
+    print(board.print())
+
+if __name__ == '__main__':
+    main()
